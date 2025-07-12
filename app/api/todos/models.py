2eeb56b07e7966a9dfd7a912689da01bbfd2f96a
Waleed Ahmad Zyadi <w.zyadi@thegarage.sa>
2025-07-12 11:07:01 +0300

    enh: relationship between user and their tasks

--- app/api/todos/models.py
+++ app/api/todos/models.py
@@ -1,6 +1,6 @@
 
 import sqlalchemy as sa
-from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import Mapped, relationship
 
 from app.api.todos.enums import Priority, Status
 from app.db.db import Base, Defaults
@@ -13,3 +13,9 @@ class Todo(Base, Defaults):
     priority: Mapped[str] = sa.Column(sa.String, nullable=False, default=Priority.LOW) # type: ignore
     status: Mapped[str] = sa.Column(sa.String, nullable=False, default=Status.PENDING)  # type: ignore
     is_deleted: Mapped[bool] = sa.Column(sa.Boolean, nullable=False, default=False)  # type: ignore
+    # ForeignKey to associate todo with a user
+    user_id: Mapped[str] = sa.Column(sa.String, sa.ForeignKey('auth_user.id'), nullable=False)  # type: ignore
+    user = relationship("User", back_populates="tasks")
+
+    
+
--- app/api/todos/services/create_task.py
+++ app/api/todos/services/create_task.py
@@ -1,3 +1,4 @@
+from app.api.auth.models import User
 from sqlalchemy.orm import Session
 
 from app.api.auth.schema import UserResponse
@@ -6,10 +7,14 @@ from app.api.todos.schema import TodoCreateRequest
 from app.celery_worker.tasks import send_email_task
 from app.common.enums import EmailTemplate
 from app.common.generate_random_id_uuid import generate_random_uuid
-
+from sqlalchemy import select
 
 def create_task_(body: TodoCreateRequest, session: Session, current_user: UserResponse):
-    task = Todo(**body.model_dump(), created_by=current_user.email) # TODO we'll add actual user over here.
+    user_stmt = select(User).where(User.email == current_user.email)
+    user: User | None = session.execute(user_stmt).scalar_one_or_none()
+    if user is None:
+        raise Exception(detail='user not found', status=401)
+    task = Todo(**body.model_dump(),user_id=user.id, created_by=current_user.email) # TODO we'll add actual user over here.
     session.add(task)
 
     send_email_task.apply_async(

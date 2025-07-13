cdc0187efd09b4361da0f891c6ebc1d00199cd2a
Waleed Ahmad Zyadi <w.zyadi@thegarage.sa>
2025-07-13 06:53:23 +0300

    feat: add documentation explain what does services do

--- app/api/todos/services/delete_task.py
+++ app/api/todos/services/delete_task.py
@@ -6,7 +6,7 @@ from app.api.todos.models import Todo
 
 
 def delete_task_(task_id: str, session: Session):
-
+    '''Delete a task by passing thier task_id'''
     stmt = select(Todo).where(Todo.id == task_id, Todo.is_deleted != True) # type: ignore
 
     task: Todo | None = session.execute(stmt).scalar_one_or_none()
